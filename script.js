8256406b52e56d8cd894f08822628bc5afaff5e6
Danykas30 <parko20a@gmail.com>
2025-02-08 21:34:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-      let scrollSpeed = 0.1; // Adjust speed (lower = smoother)
-        
-        let lastScrollTop = window.scrollY;
 
-        function smoothScroll() {
-            let currentScroll = window.scrollY;
-            let diff = currentScroll - lastScrollTop;
-            window.scrollBy(0, diff * scrollSpeed);
-            lastScrollTop = currentScroll;
-            requestAnimationFrame(smoothScroll);
-        }
-
-        smoothScroll(); // Start animation
