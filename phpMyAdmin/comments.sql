cc6b8fbbc65d754df5d68ce813dd5069949de048
Danykas30 <parko20a@gmail.com>
2025-02-11 12:50:09 +0200

    Create comments.sql

---
+++ phpMyAdmin/comments.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE comments_db;
+USE comments_db;
+
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    comment TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
