fef247d755f1a7e883810d140ba74eb48e557ba0
Danykas30 <parko20a@gmail.com>
2025-02-12 09:51:49 +0200

    Create submit_comment.php

---
+++ submit_comment.php
@@ -0,0 +1,18 @@
+<?php
+include 'config.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = htmlspecialchars($_POST['username']);
+    $comment = htmlspecialchars($_POST['comment']);
+
+    if (!empty($username) && !empty($comment)) {
+        $stmt = $conn->prepare("INSERT INTO comments (username, comment) VALUES (?, ?)");
+        $stmt->bind_param("ss", $username, $comment);
+        $stmt->execute();
+        $stmt->close();
+    }
+}
+
+header("Location: index.php");
+exit();
+?>
