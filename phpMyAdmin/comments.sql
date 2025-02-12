a2e9fce1981b8a8669c66df68bf78b0cd7626158
Danykas30 <parko20a@gmail.com>
2025-02-12 13:45:37 +0200

    Delete phpMyAdmin directory

--- phpMyAdmin/comments.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE comments_db;
-USE comments_db;
-
-CREATE TABLE comments (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    comment TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
