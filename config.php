e2ab357e7e34fa8b77d39b29a00b3e8210a2799d
Danykas30 <parko20a@gmail.com>
2025-02-12 10:39:57 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root";  // Change if using another username
-$pass = "";  // Change if you have a database password
-$dbname = "comments_db";
+$host = "localhost";  // Change this if using a remote server
+$user = "root";       // Change this to your database username
+$pass = "";           // Change this to your database password
+$dbname = "vytio_tv"; // Your database name
 
+// Connect to MySQL
 $conn = new mysqli($host, $user, $pass, $dbname);
 
+// Check for connection errors
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
